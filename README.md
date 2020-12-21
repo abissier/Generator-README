50e69b6af875d188c35b67c590b6607a5ce78851
abissier <71886314+abissier@users.noreply.github.com>
2020-12-20 17:42:53 -0800

    installed inquirer and added questions

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Generator-README
+
+### read me
+
+### Description 
+this project does x y and z 
+
+### Table of Contents
+### Installation Instructions
+### Usage
+### License
+### Contributing
+### Tests
+
+### Questions
+Email me at abg@gmail.com
+Follow me on Github <a href="abissier">abissier</a>
---
+++ index.js
@@ -0,0 +1,84 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = () =>
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "projectName",
+            message: "Project title:"
+        },
+        {
+            type: "input",
+            name: "projectDescription",
+            message: "Project description:"
+        },
+        // {
+        //     type: "input",
+        //     name: "projectInstallation",
+        //     message: "Project installation instructions:"
+        // },
+        // {
+        //     type: "input",
+        //     name: "projectUsage",
+        //     message: "Usage infromation:"
+        // },
+        // {
+        //     type: "input",
+        //     name: "projectContribution",
+        //     message: "Contribution guidelines:"
+        // },
+        // {
+        //     type: "input",
+        //     name: "projectTest",
+        //     message: "Project test instructions:"
+        // },
+        // {
+        //     type: "input",
+        //     name: "link",
+        //     message: "Deployed Link:"
+        // },
+        // {
+        //     type: "checkbox",
+        //     name: "link",
+        //     message: "license:",
+        //     choices: ["Apache License 2.0", "MIT License", "Mozzila Public License"]
+        // },
+        {
+            type: "input",
+            name: "github",
+            message: "Please enter your GitHub username:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email address (to be displayed):"
+        },
+    ]);
+
+const generateReadMe = (answers) =>
+    `
+### ${answers.projectName}
+
+### Description 
+${answers.projectDescription} 
+
+### Table of Contents
+### Installation Instructions
+### Usage
+### License
+### Contributing
+### Tests
+
+### Questions
+Email me at ${answers.email}
+Follow me on Github <a href="${answers.github}">${answers.github}</a>
+`
+
+promptUser()
+    .then((answers) => writeFileAsync('README.md', generateReadMe(answers)))
+    .then(() => console.log('Sucessfully wrote to README'))
+    .catch((err) => console.log(err));
